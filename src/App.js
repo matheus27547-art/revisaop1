a976b39ee5c59c81584ed88551696c957068b059
matheus27547-art <matheus27547@fiec.edu.br>
2026-04-09 22:44:56 -0300

    agora nao vai, mas vai assim mesmo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route,Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 import NewPage from './new/NewPage';
 import AllPage from './all/AllPage';
 
@@ -9,12 +8,14 @@ function App() {
     <div className="App">
       <Router>
         <div>
-          <Link to={'/new'}>Novo Produto</Link>
-          <Link to={'/all'}>Todos os Produtos</Link>
+          <Link to="/new">Novo Produto</Link>
+          <Link to="/all">Todos os Produtos</Link>
         </div>
+
         <Routes>
-          <Route path='/new' element={<NewPage/>} />
-          <Route path='/all' element={<AllPage/>} />
+          <Route path="/" element={<Navigate to="/new" replace />} />
+          <Route path="/new" element={<NewPage />} />
+          <Route path="/all" element={<AllPage />} />
         </Routes>
       </Router>
     </div>
--- src/new/NewPage.js
+++ src/new/NewPage.js
@@ -1,19 +1,34 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+
 import Button from './components/Button';
-import Input from './components/Input';
+
+import api from '../utils/api';
 
 
 const NewPage = props => {
+  const [form,setForm] = React.useState({nome:"", descricao:"", preco:""})
+
+  const handleChange = e => {
+    const {name, value} = e.target
+    setForm({...form, [name]: value})
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    api.post("/products", form)
+      .then(data => setForm({nome:"", descricao:"", preco:""}))
+  }
+
+
   return (
     <div>
         <h1>Novo produto</h1>
         <form>
-            <input label="Nome" id="name" name="name" onChange={""}/>
-            <input label="Descrição" id="description" name="description" onChange={""}/>
-            <input label="Preço" id="price" name="price" onChange={""}/>
+            <input label="Nome" id="name" name="name" value={form.nome} onChange={handleChange}/>
+            <input label="Descrição" id="description" name="description" value={form.descricao} onChange={handleChange}/>
+            <input label="Preço" id="price" name="price" value={form.preco} onChange={handleChange}/>
             <hr/>
-            <Button onClick={""}>
+            <Button onClick={handleSubmit}>
                 Submeter produto
             </Button>
         </form>
